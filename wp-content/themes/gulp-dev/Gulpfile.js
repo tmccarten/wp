62d8a2920e433f288803a69442d512941acb2053
tmccarten <thomasmccarten@hotmail.com>
2017-03-13 14:39:57 +0000

    update gulpfile

--- wp-content/themes/gulp-dev/Gulpfile.js
+++ wp-content/themes/gulp-dev/Gulpfile.js
@@ -1,4 +1,4 @@
-var themename = 'humescores';
+var themename = 'mccarten';
 
 var gulp = require('gulp'),
 	// Prepare and optimize code etc
@@ -26,7 +26,7 @@ gulp.task('css', function() {
 	return gulp.src(scss + '{style.scss,rtl.scss}')
 	.pipe(sourcemaps.init())
 	.pipe(sass({
-		outputStyle: 'expanded', 
+		outputStyle: 'expanded',
 		indentType: 'tab',
 		indentWidth: '1'
 	}).on('error', sass.logError))
@@ -56,9 +56,9 @@ gulp.task('javascript', function() {
 
 // Watch everything
 gulp.task('watch', function() {
-	browserSync.init({ 
+	browserSync.init({
 		open: 'external',
-		proxy: 'humescores.dev',
+		proxy: 'localhost/wp/',
 		port: 8080
 	});
 	gulp.watch([root + '**/*.css', root + '**/*.scss' ], ['css']);
