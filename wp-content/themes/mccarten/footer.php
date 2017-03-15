2e930795792a9f9f8b804cae14d5b63d5e28994b
tmccarten <thomasmccarten@hotmail.com>
2017-03-15 03:57:19 +0000

    footer

--- wp-content/themes/mccarten/footer.php
+++ wp-content/themes/mccarten/footer.php
@@ -12,14 +12,16 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
+        
+    <div class="site-footer-container">
+	<footer id="colophon" class="site-container site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mccarten' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mccarten' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mccarten' ), 'mccarten', '<a href="https://automattic.com/" rel="designer">Tom McCarten</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
+    </div><!-- site-footer-container -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
